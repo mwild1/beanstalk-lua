425d1233bf7ab56b6ec98b21781ea2a96b1746ac
Matthew Wild <mwild1@gmail.com>
2011-10-15 21:14:23 -0400

    Some re-organisation and simplification of the module.
     - Removed use of module() function
     - Created separate metatables for unconnected/connected beanstalk objects
     - Changed require of "src.beanjob" to "beanjob"

--- src/beanstalk.lua
+++ src/beanstalk.lua
@@ -1,30 +1,46 @@
-local print = print
-local setmetatable = setmetatable
-local require = require
-local beanjob = require("src.beanjob")
-local string = string
-local error = error
+local socket = require("socket")
+local beanjob = require("beanjob")
 
-module("beanstalk")
+local beanstalk = { -- Module
+	default_server = "127.0.0.1";
+	default_port = 11300;
+}
 
-beanstalk = {}
+local beanstalk_unconnected = {} -- Beanstalk object methods (when not connected)
+local beanstalk_connected = {} -- Beanstalk object methods (when connected)
+
+-- Metatables
+local beanstalk_unconnected_mt = { __index = beanstalk_unconnected }
+local beanstalk_connected_mt = { __index = beanstalk_connected }
+
+
+setmetatable(beanstalk_unconnected, { __index = function (t, k, v)
+	if beanstalk_connected[k] then
+		return function ()
+			return nil, "Not connected to beanstalkd"
+		end
+	end
+end })
 
 --Carriage-return + linefeed for writing to the socket.
 local crlf = "\r\n"
 
 --Create a new beanstalk object
-function beanstalk:new() 
+function beanstalk.new() 
 	local object = { watching = {} }
-	setmetatable(object, { __index = beanstalk })
+	setmetatable(object, beanstalk_unconnected_mt)
 	return object
 end
 
 --Connect to a server + port
-function beanstalk:connect(server, port)
-	self.connection = require( "socket" ).tcp()
-	local result, err = self.connection:connect(server,port)
+function beanstalk_unconnected:connect(server, port)
+	server = server or beanstalk.default_server
+	port = port or beanstalk.default_port
+	self.connection = socket.tcp()
+	local result, err = self.connection:connect(server, port)
 	if result ~= nil then
 		print("Successful connection to "..server..":"..port)
+		setmetatable(self, beanstalk_connected_mt)
 		return nil
 	else
 		error(err)
@@ -32,66 +48,54 @@ function beanstalk:connect(server, port)
 end
 
 --Put a job into the queue
-function beanstalk:put(data, priority)
-	if self.connection ~= nil then
-		output = "put "..priority.." 1 1 "..#data
-		self.connection:send(output..crlf..data..crlf)
-		line = self.connection:receive( "*l")
-		if starts(line,"INSERTED") then
-			print(line)
-			return nil
-		else
-			error(line)
-		end	
-	end
+function beanstalk_connected:put(data, priority)
+	output = "put "..priority.." 1 1 "..#data
+	self.connection:send(output..crlf..data..crlf)
+	line = self.connection:receive( "*l")
+	if starts(line,"INSERTED") then
+		print(line)
+		return nil
+	else
+		error(line)
+	end	
 end
 
 --Watch a tube, for reserving jobs
-function beanstalk:watch(tube)
-	if self.connection ~= nil then
-		self.connection:send("watch "..tube..crlf)
-		line = self.connection:receive( "*l")
-		if starts(line,"WATCHING") then
-			print(line)
-			return nil
-		else
-			error(line)
-		end	
-	end
+function beanstalk_connected:watch(tube)
+	self.connection:send("watch "..tube..crlf)
+	line = self.connection:receive( "*l")
+	if starts(line,"WATCHING") then
+		print(line)
+		return nil
+	else
+		error(line)
+	end	
 end
 
 --Reserve a job from the queue, return a beanjob
-function beanstalk:reserve()
-	if self.connection ~= nil then
-		self.connection:send("reserve"..crlf)
-		line = self.connection:receive( "*l")
-		print(line)
-		if starts(line,"RESERVED") then
-			id, data = string.match( line, "%S+ (%S+) (%S+)" )
-			line = self.connection:receive("*l")
-			job = beanjob:new(line, id)
-			return job
-		else
-			error(line)
-		end
-	else 
-		error("No connection detected.")
+function beanstalk_connected:reserve()
+	self.connection:send("reserve"..crlf)
+	line = self.connection:receive( "*l")
+	print(line)
+	if starts(line,"RESERVED") then
+		id, data = string.match( line, "%S+ (%S+) (%S+)" )
+		line = self.connection:receive("*l")
+		job = beanjob:new(line, id)
+		return job
+	else
+		error(line)
 	end
 end
 
 --Use a tube, for writing jobs
-function beanstalk:use(tube)
-	if self.connection ~= nil then
-		self.connection:send("use "..tube..crlf)
-		line = self.connection:receive( "*l")
-		if (starts(line,"USING")) then
-			print(line)
-			return nil
-		else 
-			error(line)
-		end
+function beanstalk_connected:use(tube)
+	self.connection:send("use "..tube..crlf)
+	line = self.connection:receive( "*l")
+	if (starts(line,"USING")) then
+		print(line)
+		return nil
 	else 
-		error("No connection")
+		error(line)
 	end
 end
 
@@ -100,4 +104,4 @@ function starts(String,Start)
    return string.sub(String,1,string.len(Start))==Start
 end
 
-return beanstalk 
+return beanstalk
